d83152876ef4429e51f44f0cbe50556ef15bcacb
Firdevs <firdes.sahin92@gmail.com>
2025-01-03 15:47:07 +0300

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <></>
+    <>
+      Hello World
+      <Image src="/icons/timer.svg" width={16} height={16} alt="timer-icon"/>
+    </>
   );
 }
